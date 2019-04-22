08cf381de05ecf39f5cab310bbe7b502d0023712
foolsogood <851809776@qq.com>
2019-04-22 19:57:44 +0800

    文档新增查看数据

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,61 +1,98 @@
 <template>
   <div id="app" style="padding:20px 30px">
-    <p style="line-height:60px">staticTable</p>
-    <staticTable :tableData="tableData"/>
-    <p style="line-height:60px">rowEditableTable</p>
-    <p>
-      <span
-        @click="toggleEdit(item)"
-        style="padding:0 20px;cursor:pointer;line-height:40px"
-        :style="rowEdit.cur==item.title?{color:'red'}:{}"
-        v-for="(item,idx) in rowEdit.list"
-        :key="idx"
-      >{{item.title}}</span>
-    </p>
-    <rowEditableTable
-      @TableDataChange="rowEditTableDataChangeHandler"
-      :tableData="edit_tableData"
-      :tableHeader="edit_tableHeader"
-      :bodyNotShowProps="['code']"
-      uniqueKey="code"
-    />
-    <p style="line-height:60px">mutilTable</p>
-    <p>
-      <span
-        @click="toggle(item)"
-        style="padding:0 20px;cursor:pointer;line-height:40px"
-        :style="curTitle==item.title?{color:'red'}:{}"
-        v-for="(item,idx) in list"
-        :key="idx"
-      >{{item.title}}</span>
-    </p>
-    <mutilTable
-      @TableDataChange="changeDataHandler"
-      :firstThStyle="{color:'#ff0000'}"
-      :firstThClickHandler="triggerFn"
-      :isFirstThEableClick="true"
-      :isReadOnly="isMutilReadOnly"
-      :tableData="tableDatas_1"
-      :tableHeader="tableHeader_1"
-      :bodyNotShowProps="['code','id']"
-      uniqueKey="code"
-    />
-    <!-- <p style="line-height:60px">参数说明</p>
+    <div class="bd-btm-2 pd-btm-30">
+      <p class="lh-60">staticTable</p>
+      <staticTable :tableData="tableData"/>
+      <div>
+        <button @click="view1Handler" class="btn">{{showText(isView1Show)}}</button>
+        <div v-show="isView1Show">
+          <JsonViewer :value="tableData" :expand-depth="5" copyable sort></JsonViewer>
+        </div>
+      </div>
+    </div>
+    <div class="bd-btm-2 pd-btm-30">
+      <p class="lh-60">rowEditableTable</p>
+      <p>
+        <span
+          @click="toggleEdit(item)"
+          style="padding:0 20px;cursor:pointer;line-height:40px"
+          :style="rowEdit.cur==item.title?{color:'red'}:{}"
+          v-for="(item,idx) in rowEdit.list"
+          :key="idx"
+        >{{item.title}}</span>
+      </p>
+      <rowEditableTable
+        @TableDataChange="rowEditTableDataChangeHandler"
+        :tableData="edit_tableData"
+        :tableHeader="edit_tableHeader"
+        :bodyNotShowProps="['code']"
+        uniqueKey="code"
+      />
+      <div>
+        <button @click="view2Handler" class="btn">{{showText(isView2Show)}}</button>
+        <div v-show="isView2Show">
+          <div>表头
+            <JsonViewer :value="isView2Show&&edit_tableHeader" :expand-depth="5" copyable sort></JsonViewer>
+          </div>
+          <div>表体
+            <JsonViewer :value="isView2Show&&edit_tableData" :expand-depth="5" copyable sort></JsonViewer>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pd-btm-30">
+      <p class="lh-60">mutilTable</p>
+      <p>
+        <span
+          @click="toggle(item)"
+          style="padding:0 20px;cursor:pointer;line-height:40px"
+          :style="curTitle==item.title?{color:'red'}:{}"
+          v-for="(item,idx) in list"
+          :key="idx"
+        >{{item.title}}</span>
+      </p>
+      <mutilTable
+        @TableDataChange="changeDataHandler"
+        :firstThStyle="{color:'#ff0000'}"
+        :firstThClickHandler="triggerFn"
+        :isFirstThEableClick="true"
+        :isReadOnly="isMutilReadOnly"
+        :tableData="mutil_tableData"
+        :tableHeader="mutil_tableHeader"
+        :bodyNotShowProps="['code','id']"
+        uniqueKey="code"
+      />
+      <div>
+        <button @click="view3Handler" class="btn">{{showText(isView3Show)}}</button>
+        <div v-show="isView3Show">
+          <div>表头
+            <JsonViewer :value="isView3Show&&mutil_tableHeader" :expand-depth="5" copyable sort></JsonViewer>
+          </div>
+          <div>表体
+            <JsonViewer :value="isView3Show&&mutil_tableData" :expand-depth="5" copyable sort></JsonViewer>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <p class="lh-60">参数说明</p>
     <explainCom :tableData="explain.prop.data" :tableHeader="explain.prop.header" />
-    <p style="line-height:60px">回调方法说明</p>
+    <p class="lh-60">回调方法说明</p>
     <explainCom :tableData="explain.func.data" :tableHeader="explain.func.header" />
-    <p style="line-height:60px">直接操作节点方法说明</p>
-    <explainCom :tableData="explain.method.data" :tableHeader="explain.method.header" /> -->
+    <p class="lh-60">直接操作节点方法说明</p>
+    <explainCom :tableData="explain.method.data" :tableHeader="explain.method.header" />-->
   </div>
 </template>
 <script>
 import { staticTable, rowEditableTable, mutilTable } from "custom-ele-table";
 // import explainCom from "./explain";
-
+import JsonViewer from "vue-json-viewer";
 export default {
   name: "App",
   data() {
     return {
+      isView1Show: false,
+      isView2Show: false,
+      isView3Show: false,
       triggerFn: () => {
         console.log("表头首位被点击");
       },
@@ -73,7 +110,7 @@ export default {
       //     header: require("./mock/explain").default.methodTableHeader
       //   }
       // },
-      tableData:require("./mock/baseInfo").tableBody ,
+      tableData: require("./mock/baseInfo").tableBody,
       edit_tableData: [],
       edit_tableHeader: [],
       rowEdit: {
@@ -92,8 +129,8 @@ export default {
         ]
       },
       curTitle: "table3-计算-多行表头",
-      tableDatas_1: [],
-      tableHeader_1: [],
+      mutil_tableData: [],
+      mutil_tableHeader: [],
       isMutilReadOnly: false,
       list: [
         {
@@ -121,14 +158,15 @@ export default {
     staticTable,
     rowEditableTable,
     mutilTable,
+    JsonViewer
     // explainCom
   },
   watch: {
     curTitle: {
       handler(val) {
         const _temp = this.list.find(item => item.title == val);
-        this.tableDatas_1 = _temp.data;
-        this.tableHeader_1 = _temp.header;
+        this.mutil_tableData = _temp.data;
+        this.mutil_tableHeader = _temp.header;
         this.isMutilReadOnly = _temp.isReadOnly;
       },
       immediate: true
@@ -143,6 +181,9 @@ export default {
     }
   },
   methods: {
+    showText(bool) {
+      return bool ? "隐藏数据" : "查看数据";
+    },
     toggle(item) {
       this.curTitle = item.title;
     },
@@ -152,8 +193,35 @@ export default {
     changeDataHandler(data) {
       // console.log(data);
     },
+    view1Handler() {
+      this.isView1Show = !this.isView1Show;
+    },
+    view2Handler() {
+      this.isView2Show = !this.isView2Show;
+    },
+    view3Handler() {
+      this.isView3Show = !this.isView3Show;
+    },
     rowEditTableDataChangeHandler(data) {}
   }
 };
 </script>
+<style>
+.pd-btm-30 {
+  padding-bottom: 30px;
+}
+.bd-btm-2 {
+  border-bottom: 2px dashed #ddd;
+}
+.lh-60 {
+  line-height: 60px;
+}
+.btn{
+  cursor: pointer;
+  border:1px solid #ddd;
+  padding: 2px 5px;
+  margin-bottom: 10px;
+}
+</style>
+
 
